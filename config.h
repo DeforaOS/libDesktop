608e360bd36c56abf87e7fdd19558a56a53f26d0
Pierre Pronchery <khorben@defora.org>
2013-12-22 18:00:21 +0100

    About to release DeforaOS libDesktop 0.1.0

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 #define PACKAGE "libDesktop"
-#define VERSION "0.0.10"
+#define VERSION "0.1.0"
 
 #ifndef PREFIX
 # define PREFIX "/usr/local"
--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 PACKAGE="libDesktop"
-VERSION="0.0.10"
+VERSION="0.1.0"
 
 PREFIX="/usr/local"
 LIBDIR="${PREFIX}/lib"
