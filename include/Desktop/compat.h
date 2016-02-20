1496dce23fc6e2c675cb7858261cb208654fe853
Pierre Pronchery <khorben@defora.org>
2016-02-20 03:20:30 +0100

    Fix compatibility layer for Gtk+ 2

--- include/Desktop/compat.h
+++ include/Desktop/compat.h
@@ -103,7 +103,7 @@
 # endif
 
 /* Gtk+ 3.0 */
-# if GTK_CHECK_VERSION(3, 0, 0)
+# if !GTK_CHECK_VERSION(3, 0, 0)
 void gdk_window_clear(GdkWindow * window);
 
 GtkWidget * gtk_box_new(GtkOrientation orientation, gint spacing);
